1ec5991f39f4525d4f87596650d3f3160890ea33
marcaded <marcaded@msu.edu>
2023-07-03 00:50:24 -0400

    Final Commit

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-in_file = Path("Resources/election_Data.csv")
+
 def open_file(fp):
     ccs_count = 0
     dd_count = 0
@@ -21,17 +21,48 @@ def total(countc, countd, countr):
     ccs_percentage = (countc/total)*100
     dd_percentage = (countd/total)*100
     ra_percentage = (countr/total)*100
+    winner = ' '
+    if ccs_percentage > dd_percentage and ccs_percentage> ra_percentage:
+        winner = "Charles Casper Stockham"
+        
+    elif dd_percentage > ra_percentage and dd_percentage > ccs_percentage:
+        winner = "Diana DeGette"
+    else:
+        winner = "Raymon Anthony Doane"
     
-    return total, ccs_percentage, dd_percentage, ra_percentage
+    return total, ccs_percentage, dd_percentage , ra_percentage, winner
+    
 
 
-# def main():
-#     in_file = Path("Resources/election_Data.csv")
+def main():
+    in_file = Path("Resources/election_data.csv")
+    out_file = Path("Analysis/Analysis.txt")
+    ccs_count, dd_count, rad_count = open_file(in_file)
+    totals, ccs_percentage, dd_percentage , ra_percentage, winner = total(ccs_count, dd_count, rad_count)
+    print("Election Results")
+    print('-------------------')
+    print("Total Votes: {}".format(totals))
+    print('--------------------')
+    print("Charles Casper Stockham: {:.3f}% ({})".format(ccs_percentage, ccs_count))
+    print("Diana DeGette: {:.3f}% ({})".format(dd_percentage, dd_count))
+    print("Raymon Anthony Doane: {:.3f}% ({})".format(ra_percentage, rad_count))
+    print("----------------------")
+    print("Winner: {}".format(winner))
+    with open(out_file, 'w') as fpath:
+        fpath.write("Election Results")
+        fpath.write('-------------------')
+        fpath.write("Total Votes: {}".format(totals))
+        fpath.write('--------------------')
+        fpath.write("Charles Casper Stockham: {:.3f}% ({})".format(ccs_percentage, ccs_count))
+        fpath.write("Diana DeGette: {:.3f}% ({})".format(dd_percentage, dd_count))
+        fpath.write("Raymon Anthony Doane: {:.3f}% ({})".format(ra_percentage, rad_count))
+        fpath.write("----------------------")
+        fpath.write("Winner: {}".format(winner))
 
 
 
 
-# #if __name__ == main():
-#     main()
 
-print(open_file(in_file))
+
+if __name__ == main():
+    main()
